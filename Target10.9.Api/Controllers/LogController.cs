0524445545d2d45c4a9961418a7f3c528c1da92c
romainavinenc <romain.avinenc@gmail.com>
2025-07-14 16:14:15 +0200

    T19-68 : ajout route - voir un log specifique

--- Target10.9.Api/Controllers/LogController.cs
+++ Target10.9.Api/Controllers/LogController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Target10._9.Business.Logs;
+using Target10._9.Business.Logs.Queries;
 
 namespace Target10._9_api.Controllers;
 
@@ -20,6 +21,16 @@ public class LogController(
     {
         var logs = await logsService.GetLogsAsync(User, cancellationToken);
         return Ok(logs);
+    }
+    
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetLogById(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetLogByIdQuery { Id = id };
+        var log = await logsService.GetLogByIdAsync(query, User, cancellationToken);
+        return Ok(log);
     }
     
     #endregion
---
+++ Target10.9.Business/Logs/Queries/GetLogByIdQuery.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+
+namespace Target10._9.Business.Logs.Queries;
+
+public class GetLogByIdQuery
+{
+    public Guid Id { get; set; }
+}
+
+public class GetLogByIdQueryValidator : AbstractValidator<GetLogByIdQuery>
+{
+    public GetLogByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("L'id est requis.");
+    }
+}
