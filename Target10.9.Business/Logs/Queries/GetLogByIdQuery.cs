afcbcc949923f8a8356f310d5a6477ea3387bfb8
romainavinenc <romain.avinenc@gmail.com>
2025-07-14 18:06:25 +0200

    fix PR

--- Target10.9.Business/Logs/Queries/GetLogByIdQuery.cs
+++ Target10.9.Business/Logs/Queries/GetLogByIdQuery.cs
@@ -13,6 +13,6 @@ public class GetLogByIdQueryValidator : AbstractValidator<GetLogByIdQuery>
     {
         RuleFor(x => x.Id)
             .NotEmpty()
-            .WithMessage("l'Id is required.");
+            .WithMessage("ID is required.");
     }
 }
