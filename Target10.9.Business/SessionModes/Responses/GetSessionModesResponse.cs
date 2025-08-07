50203a508695bd80c70f80582c07479ac9404bd8
romainavinenc <romain.avinenc@gmail.com>
2025-08-07 10:50:17 +0100

    WIP

--- Target10.9.Business/SessionModes/Responses/GetSessionModesResponse.cs
+++ Target10.9.Business/SessionModes/Responses/GetSessionModesResponse.cs
@@ -2,6 +2,7 @@ namespace Target10._9.Business.SessionModes.Responses;
 
 public class GetSessionModesResponse
 {
+    public Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public TimeOnly TimeLimits { get; set; }
     public TimeOnly WarmUp { get; set; }
