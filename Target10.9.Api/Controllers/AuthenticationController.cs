b077f3e5a07ac165b233a49e302427b459202ee4
DukMik <dukmik.game@gmail.com>
2025-10-30 11:24:45 +0100

    soum soum test

--- Target10.9.Api/Controllers/AuthenticationController.cs
+++ Target10.9.Api/Controllers/AuthenticationController.cs
@@ -24,7 +24,7 @@ namespace Target10._9_api.Controllers
             }
             catch (InvalidOperationException ex)
             {
-                return Conflict(new { message = "This email is already in use ::::::" + ex.Message });
+                return Conflict(new { message = "Ce mail est déja utilisée  ::::::" + ex.Message });
             }
             catch (ArgumentException ex)
             {
